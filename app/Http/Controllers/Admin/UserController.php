85c10451790a164f0f20c5dd6cf037e29cd44af6
changlongz <changlongz@lineim.com>
2019-06-03 14:45:25 +0800

    初始化

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use App\Services\Helper;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        // 获取参数
+        $current   = intval($request->get('current',1));
+        $pageSize  = intval($request->get('pageSize',10));
+        $search    = $request->get('search');
+
+        // 定义对象
+        $query = User::query();
+
+        // 查询
+        if(!empty($search)) {
+
+            // 用户名
+            if(isset($search['username'])) {
+                $query->where('name','like','%'.$search['username'].'%');
+            }
+
+            // 昵称
+            if(isset($search['nickname'])) {
+                $query->where('nickname','like','%'.$search['nickname'].'%');
+            }
+
+            // 手机号
+            if(isset($search['phone'])) {
+                $query->where('phone','like','%'.$search['phone'].'%');
+            }
+
+            // 状态
+            if(isset($search['status'])) {
+                if(!empty($search['status'])) {
+                    $query->where('status',$search['status']);
+                }
+            }
+
+            // 时间范围
+            if(isset($search['dateRange'])) {
+                if(!empty($search['dateRange'][0]) || !empty($search['dateRange'][1])) {
+                    $query->whereBetween('created_at', [$search['dateRange'][0], $search['dateRange'][1]]);
+                }
+            }
+        }
+
+        // 查询数量
+        $total = $query
+        ->where('status', '>', 0)
+        ->count();
+
+        // 查询列表
+        $lists = $query
+        ->skip(($current-1)*$pageSize)
+        ->take($pageSize)
+        ->where('status', '>', 0)
+        ->orderBy('id', 'desc')
+        ->get()
+        ->toArray();
+
+        foreach ($lists as $key => $value) {
+            $lists[$key]['cover_path'] = Helper::getPicture($value['cover_id']);
+        }
+
+        // 默认页码
+        $pagination['defaultCurrent'] = 1;
+        // 当前页码
+        $pagination['current'] = $current;
+        // 分页数量
+        $pagination['pageSize'] = $pageSize;
+        // 总数量
+        $pagination['total'] = $total;
+
+        // 模板数据
+        $data['lists'] = Helper::listsFormat($lists);
+        if(!empty($data)) {
+            return $this->success('获取成功！','',$data,$pagination,$search);
+        } else {
+            return $this->success('获取失败！');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
